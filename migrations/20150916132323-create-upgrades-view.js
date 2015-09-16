f1cd52bfa441d17db93a234df86fc095b1198843
Brian Wigginton <brianwigginton@gmail.com>
2015-09-16 09:07:35 -0500

    add upgrades view to db

---
+++ migrations/20150916132323-create-upgrades-view.js
@@ -0,0 +1,10 @@
+var dbm = global.dbm || require('db-migrate');
+
+exports.up = function(db, callback) {
+  var sql = 'create view upgrades as select b.id, b.name, bl.level, bl.time, bl.gold, bl.wood, bl.stone, bl.iron, bl.experience, (1.0 * bl.experience / bl.time * 60) as exp_per_hour from buildings b join building_levels bl on bl.building_id = b.id order by b.name, bl.level';
+  db.runSql(sql, callback);
+};
+
+exports.down = function(db, callback) {
+  db.runSql('drop view upgrades;', callback);
+};
