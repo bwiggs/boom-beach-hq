7dcf41e6c742dcd2200c92a06a2c0c2b85e44eb9
Brian Wigginton <brianwigginton@gmail.com>
2015-09-17 09:20:14 -0500

    upgrades view should show exp_hr with 2 decomal places

--- migrations/20150916132323-create-upgrades-view.js
+++ migrations/20150916132323-create-upgrades-view.js
@@ -1,7 +1,7 @@
 var dbm = global.dbm || require('db-migrate');
 
 exports.up = function(db, callback) {
-  var sql = 'create view upgrades as select b.id, b.name, bl.level, bl.time, bl.required_hq, bl.gold, bl.wood, bl.stone, bl.iron, bl.experience, (1.0 * bl.experience / bl.time * 60) as exp_per_hour from buildings b join building_levels bl on bl.building_id = b.id order by b.name, bl.level';
+  var sql = 'create view upgrades as select b.id, b.name, bl.level, bl.time, bl.required_hq, bl.gold, bl.wood, bl.stone, bl.iron, bl.experience, printf("%.2f", (1.0 * bl.experience / bl.time * 60)) as exp_per_hour from buildings b join building_levels bl on bl.building_id = b.id order by b.name, bl.level';
   db.runSql(sql, callback);
 };
 
