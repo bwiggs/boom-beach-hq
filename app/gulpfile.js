e14c5f521804ed87a10e3c613cca924f3ba6c10d
Brian Wigginton <brianwigginton@gmail.com>
2015-09-18 17:52:00 -0500

    adds gulp for dev builds and babel! yay!

---
+++ app/gulpfile.js
@@ -0,0 +1,55 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var babel = require('gulp-babel');
+var watch = require('gulp-watch');
+var batch = require('gulp-batch');
+
+var deps = [
+  'bower_components/react/react.min.js',
+  'bower_components/react/JSXTransformer.js',
+];
+
+var app = [
+  'src/**/*.js'
+];
+
+gulp.task('dep-scripts', function() {
+  return gulp.src(deps)
+    .pipe(concat('deps.js'))
+    .pipe(gulp.dest('./dist/scripts'));
+});
+
+gulp.task('app-scripts', function() {
+  return gulp.src(app)
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./dist/scripts'));
+});
+
+gulp.task('es6to5', function () {
+  return gulp.src(app)
+    .pipe(babel())
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./dist/scripts'));
+});
+
+gulp.task('sass', function () {
+  gulp.src('assets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('assets/sass/**/*.scss', ['sass']);
+});
+
+gulp.task('watch', function () {
+    watch('src/**/*.js', batch(function (events, done) {
+        gulp.start('build', done);
+    }));
+});
+
+gulp.task('build', ['sass', 'dep-scripts', 'es6to5']);
+gulp.task('default', ['build']);
+
+gulp.task('dev', ['build', 'watch', 'sass:watch']);
--- app/src/upgrades.js
+++ app/src/upgrades.js
@@ -2,13 +2,12 @@ var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('bbhq.db');
 
 var Upgrades = React.createClass({
-  render: function() {
+  render: () => {
     return  <h1>Upgrades</h1>;
   }
 });
 
-db.each("select * from upgrades", function(err, upgrades) {
+db.each("select * from upgrades", (err, upgrades) => {
   console.log('render!');
   React.render(new Upgrades({upgrades: upgrades}), document.getElementById('main'));
 });
-
